83bb7cab505ffd3c7879cf773224fbbdc4b58cdb
Tyler Fisher <tfisher@politico.com>
2017-10-06 11:16:19 -0400

    initial django app

---
+++ fabfile/__init__.py
@@ -0,0 +1,79 @@
+import server_config
+
+from fabric.api import local, settings, task
+from fabric.state import env
+
+# Other fabfiles
+from . import servers
+
+"""
+Base configuration
+"""
+env.user = server_config.SERVER_USER
+env.forward_agent = True
+
+env.hosts = []
+
+
+"""
+Environments
+Changing environment requires a full-stack test.
+An environment points to both a server and an S3
+bucket.
+"""
+
+
+@task
+def production():
+    """
+    Run as though on production.
+    """
+    env.settings = 'production'
+    server_config.configure_targets(env.settings)
+    env.hosts = server_config.SERVERS
+
+
+@task
+def staging():
+    """
+    Run as though on staging.
+    """
+    env.settings = 'staging'
+    server_config.configure_targets(env.settings)
+    env.hosts = server_config.SERVERS
+
+"""
+Branches
+
+Changing branches requires deploying that branch to a host.
+"""
+
+
+@task
+def stable():
+    """
+    Work on stable branch.
+    """
+    env.branch = 'stable'
+
+
+@task
+def master():
+    """
+    Work on development branch.
+    """
+    env.branch = 'master'
+
+
+@task
+def branch(branch_name):
+    """
+    Work on any specified branch.
+    """
+    env.branch = branch_name
+
+
+@task
+def deploy_server():
+    servers.checkout_latest()
+    servers.restart_service('uwsgi')
---
+++ server_config.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+"""
+Project-wide application configuration.
+
+DO NOT STORE SECRETS, PASSWORDS, ETC. IN THIS FILE.
+They will be exposed to users. Use environment variables instead.
+See get_secrets() below for a fast way to access them.
+"""
+
+import logging
+import os
+
+"""
+NAMES
+"""
+# Project name to be used in urls
+# Use dashes, not underscores!
+PROJECT_SLUG = 'elections'
+
+# Project name to be used in file paths
+PROJECT_FILENAME = 'elections'
+
+# The name of the repository containing the source
+REPOSITORY_NAME = 'elections'
+GITHUB_USERNAME = 'The-Politico'
+REPOSITORY_URL = 'git@github.com:%s/%s.git' % (GITHUB_USERNAME, REPOSITORY_NAME)
+REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
+
+PRODUCTION_SERVERS = ['']
+STAGING_SERVERS = ['']
+
+SERVER_USER = 'ubuntu'
+SERVER_PYTHON = 'python3'
+SERVER_PROJECT_PATH = '/home/%s/apps/%s' % (SERVER_USER, PROJECT_FILENAME)
+SERVER_REPOSITORY_PATH = '%s/repository' % SERVER_PROJECT_PATH
+SERVER_VIRTUALENV_PATH = '%s/virtualenv' % SERVER_PROJECT_PATH
+
+UWSGI_SOCKET_PATH = '/run/uwsgi/%s.uwsgi.sock' % PROJECT_FILENAME
+
+# Services are the server-side services we want to enable and configure.
+# A three-tuple following this format:
+# (service name, service deployment path, service config file extension)
+SERVER_SERVICES = [
+    ('app', '/etc/uwsgi/sites', 'ini'),
+    ('uwsgi', '/etc/init', 'conf'),
+    ('nginx', '/etc/nginx/sites-available', 'conf'),
+]
+
+# These variables will be set at runtime. See configure_targets() below
+SERVERS = []
+SERVER_BASE_URL = None
+SERVER_LOG_PATH = None
+DEBUG = True
+
+"""
+Logging
+"""
+LOG_FORMAT = '%(levelname)s:%(name)s:%(asctime)s: %(message)s'
+
+
+"""
+Utilities
+"""
+def get_secrets():
+    """
+    A method for accessing our secrets.
+    """
+    secrets_dict = {}
+
+    for k,v in os.environ.items():
+        if k.startswith(PROJECT_FILENAME):
+            k = k[len(PROJECT_FILENAME) + 1:]
+            secrets_dict[k] = v
+
+    return secrets_dict
+
+
+def configure_targets(deployment_target):
+    """
+    Configure deployment targets. Abstracted so this can be
+    overriden for rendering before deployment.
+    """
+    global SERVERS
+    global SERVER_BASE_URL
+    global SERVER_LOG_PATH
+    global DEBUG
+    global DEPLOYMENT_TARGET
+    global LOG_LEVEL
+
+    secrets = get_secrets()
+
+    if deployment_target == 'production':
+        SERVERS = PRODUCTION_SERVERS
+        SERVER_BASE_URL = 'http://%s/%s' % (SERVERS[0], PROJECT_SLUG)
+        SERVER_LOG_PATH = '/var/log/%s' % PROJECT_FILENAME
+        LOG_LEVEL = logging.WARNING
+        DEBUG = False
+    elif deployment_target == 'staging':
+        SERVERS = STAGING_SERVERS
+        SERVER_BASE_URL = 'http://%s/%s' % (SERVERS[0], PROJECT_SLUG)
+        SERVER_LOG_PATH = '/var/log/%s' % PROJECT_FILENAME
+        LOG_LEVEL = logging.DEBUG
+        DEBUG = True
+    else:
+        SERVERS = []
+        SERVER_BASE_URL = 'http://127.0.0.1:8001/%s' % PROJECT_SLUG
+        SERVER_LOG_PATH = '/tmp'
+        LOG_LEVEL = logging.DEBUG
+        DEBUG = True
+
+    DEPLOYMENT_TARGET = deployment_target
+
+"""
+Run automated configuration
+"""
+DEPLOYMENT_TARGET = os.environ.get('DEPLOYMENT_TARGET', None)
+
+configure_targets(DEPLOYMENT_TARGET)
