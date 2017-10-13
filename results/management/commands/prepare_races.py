cce056e97db24ced4da86c8c54491b740483d9ca
Tyler Fisher <tfisher@politico.com>
2017-10-13 12:14:33 -0400

    get raceids for states and offices

--- results/management/commands/prepare_races.py
+++ results/management/commands/prepare_races.py
@@ -2,25 +2,49 @@ import json
 
 from django.core.management.base import BaseCommand, CommandError
 from results.models import Race
+from uuslug import slugify
 
 class Command(BaseCommand):
     help = 'finds race ids necessary for pages'
 
     def add_arguments(self, parser):
         parser.add_argument('election_date', type=str)
-        parser.add_argument('state_name', type=str)
 
     def handle(self, *args, **options):
         races = Race.objects.filter(
             election__date=options['election_date'],
-            seat__geography__label=options['state_name'].capitalize()
         )
+        states = races.values('seat__geography__label').distinct()
+        offices = races.values('seat__office__label').distinct()
 
-        ids = []
-        for race in races:
-            ids.append(race.ap_race_id)
+        for state_obj in states:
+            state = state_obj['seat__geography__label']
+            state_races = races.filter(
+                seat__geography__label=state
+            )
+            race_ids = []
 
-        with open('scripts/{0}-races.json'.format(
-            options['state_name']
-        ), 'w') as f:
-            json.dump(ids, f)
+            for race in state_races:
+                race_ids.append(race.ap_race_id)
+
+            with open(
+                'scripts/{0}-races.json'.format(slugify(state)), 
+                'w'
+            ) as f:
+                json.dump(race_ids, f)
+
+        for office_obj in offices:
+            office = office_obj['seat__office__label']
+            office_races = races.filter(
+                seat__office__label=office
+            )
+            race_ids = []
+
+            for race in office_races:
+                race_ids.append(race.ap_race_id)
+
+            with open(
+                'scripts/{0}-races.json'.format(slugify(office)),
+                'w'
+            ) as f:
+                json.dump(race_ids, f)
--- scripts/results.sh
+++ scripts/results.sh
@@ -3,31 +3,32 @@
 # grab elex results for everything
 elex results 2016-11-08 --national-only -o json > master.json
 
-# get the raceids we care about
-races=`cat scripts/alabama-races.json`
 
-# filter results
-cat master.json \
-| jq -c --argjson races "$races" '[
-  .[] |
-  select(.raceid as $id | $races|index($id)) |
-  {
-    fipscode: .fipscode,
-    first: .first,
-    last: .last,
-    level: .level,
-    officename: .officename,
-    party: .party,
-    polid: .polid,
-    polnum: .polnum,
-    precinctsreporting: .precinctsreporting,
-    precinctsreportingpct: .precinctsreportingpct,
-    precinctstotal: .precinctstotal,
-    statepostal: .statepostal,
-    votecount: .votecount,
-    votepct: .votepct,
-    winner: .winner
-  }
-]' # gzip and copy to s3 after this
+for file in ./scripts/*.json ; do
+  if [ -e "$file" ] ; then
+    races=`cat $file`
+    # filter results
+    cat master.json \
+    | jq -c --argjson races "$races" '[
+      .[] |
+      select(.raceid as $id | $races|index($id)) |
+      {
+        fipscode: .fipscode,
+        level: .level,
+        party: .party,
+        polid: .polid,
+        polnum: .polnum,
+        precinctsreporting: .precinctsreporting,
+        precinctsreportingpct: .precinctsreportingpct,
+        precinctstotal: .precinctstotal,
+        raceid: .raceid,
+        statepostal: .statepostal,
+        votecount: .votecount,
+        votepct: .votepct,
+        winner: .winner
+      }
+    ]' > $file # gzip and copy to s3 after this
+  fi
+done
 
 # take master results and insert to django
