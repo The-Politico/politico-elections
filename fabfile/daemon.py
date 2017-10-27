d60a94cfb69d1a00823263e01b1ce591ffab717e
Tyler Fisher <tfisher@politico.com>
2017-10-27 16:46:13 -0400

    attempt ap-deja-vu

--- fabfile/daemon.py
+++ fabfile/daemon.py
@@ -31,31 +31,45 @@ def main(run_once=False):
         now = time()
 
         if (now - results_start) > server_config.DAEMON_INTERVAL:
-            if not os.path.exists('output'):
-                os.makedirs('output')
-
             results_start = now
-            local('bash scripts/results.sh')
-
-            if not os.path.exists('scripts/times_run.json'):
-                with open('scripts/times_run.json', 'w') as writefile:
-                    json.dump({
-                        'times_run': 0
-                    }, writefile)
-
-            with open('scripts/times_run.json') as readfile:
-                data = json.load(readfile)
-                data['times_run'] += 1
-                print(data['times_run'])
-
-            if data['times_run'] % 10 == 0:
-                local('python manage.py update_results')
-
-            with open('scripts/times_run.json', 'w') as writefile:
-                json.dump(data, writefile)
+            process()
 
         if run_once:
             print('run once specified, exiting')
             sys.exit(0)
 
         sleep(1)
+
+
+def process():
+    if '--test' in server_config.ELEX_FLAGS:
+        print('writing test files')
+        test_path = '/tmp/ap-elex-recordings/{0}/national'.format(
+            server_config.CURRENT_ELECTION
+        )
+        os.environ['ELEX_RECORDING_DIR'] = test_path
+
+        if not os.path.exists(test_path):
+            os.makedirs(test_path)
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    local('bash scripts/results.sh')
+
+    if not os.path.exists('scripts/times_run.json'):
+        with open('scripts/times_run.json', 'w') as writefile:
+            json.dump({
+                'times_run': 0
+            }, writefile)
+
+    with open('scripts/times_run.json') as readfile:
+        data = json.load(readfile)
+        data['times_run'] += 1
+        print(data['times_run'])
+
+    if data['times_run'] % 10 == 0:
+        local('python manage.py update_results')
+
+    with open('scripts/times_run.json', 'w') as writefile:
+        json.dump(data, writefile)
