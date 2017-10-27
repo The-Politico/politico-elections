8ab7840f9ec94ba4411acfa68f0497917f2c60a1
Jon McClure <jmcclure@politico.com>
2017-10-26 21:47:08 -0400

    working module

--- showtime/staticapp/src/js/race-app/components/ResultsBar.jsx
+++ showtime/staticapp/src/js/race-app/components/ResultsBar.jsx
@@ -1,31 +1,71 @@
 import React from 'react';
+import CandidateResultsBar from 'candidate-results-bar';
+import _ from 'lodash';
 
-const ResultsBar = (props) => {
-  const db = props.session;
+// Initialize the chart
+const chart = CandidateResultsBar();
 
-  const election = db.Election.first();
-  if (!election) return null;
+class ResultsBar extends React.Component {
+  /**
+   * constructor lets us bind custom methods to our component class.
+   */
+  constructor(props) {
+    super(props);
+    // Bind our custom methods (using ES7 bind syntax "::").
+    this.drawChart = ::this.drawChart;
+    this.fetchData = ::this.fetchData;
+  }
 
-  const state = db.Division
-    .filter(d =>
-      d.level === 'state' &&
-      d.code === window.appConfig.stateFips)
-    .toModelArray();
-  const counties = db.Division
-    .filter(d => d.level === 'county')
-    .toModelArray();
+  // Called first time our component is mounted, i.e., just once.
+  componentDidMount() {
+    this.drawChart();
 
-  const results = election.serializeResults(state);
-  const countyResults = election.serializeResults(counties);
+    // Attach a resize func here!
+    window.onresize = _.debounce(() => {
+      chart.resize();
+    }, 400);
+  }
 
-  console.log('State results', results);
-  console.log('County results', countyResults);
+  // Called every time our component's props update,
+  // i.e., whenever our data updates.
+  componentDidUpdate() {
+    this.drawChart();
+  }
 
-  return (
-    <div className="results-bar">
-      <h3>😋 <purple>||||||||||||||</purple><green>-------</green> 😡</h3>
-    </div>
-  );
-};
+  // Gets data from our client database
+  fetchData() {
+    const db = this.props.session;
+
+    const election = db.Election.first();
+    if (!election) return null;
+
+    const state = db.Division
+      .filter(d =>
+        d.level === 'state' &&
+        d.code === window.appConfig.stateFips)
+      .toModelArray();
+
+    return election.serializeResults(state);
+  }
+
+  // Calls our chart's create function.
+  // (Must be able to be called multiple times, i.e., idempotent charts!)
+  drawChart() {
+    const results = this.fetchData();
+
+    if (!results) return;
+
+    chart.create('#candidateResultsBar', results.divisions.VA);
+  }
+
+  // START HERE
+  render() {
+    return (
+      <div className="results-bar">
+        <div id="candidateResultsBar" />
+      </div>
+    );
+  }
+}
 
 export default ResultsBar;
