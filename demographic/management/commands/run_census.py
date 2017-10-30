8b59d95d3c7c0ac728fe3a1fd935fdf7c900a8b4
Jon McClure <jmcclure@politico.com>
2017-10-30 10:52:10 -0400

    census command

--- demographic/management/commands/run_census.py
+++ demographic/management/commands/run_census.py
@@ -1,21 +1,39 @@
+import json
 import os
 import statistics
 
+import boto3
 from census import Census
 from django.core.management.base import BaseCommand
 from tqdm import tqdm
 
+from core.constants import DIVISION_LEVELS
 from demographic.models import CensusEstimate, CensusTable
 from geography.models import Division, DivisionLevel
 
 census = Census(os.getenv('CENSUS_API_KEY'))
 
-STATE_LEVEL = DivisionLevel.objects.get(name='state')
+STATE_LEVEL = DivisionLevel.objects.get(name=DIVISION_LEVELS['state'])
+COUNTY_LEVEL = DivisionLevel.objects.get(name=DIVISION_LEVELS['county'])
+
+OUTPUT_PATH = 'data/us-census'
+
+CACHE_HEADER = str('max-age=300')
+
+
+def get_bucket():
+    session = boto3.session.Session(
+        region_name='us-east-1',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+    )
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(os.getenv('AWS_S3_PUBLISH_BUCKET'))
+    return bucket
 
 
 class Command(BaseCommand):
-    help = 'Gathers census data from the Census API and outputs a master json \
-    by county'
+    help = 'Gathers census data from the Census API and outputs JSON to S3.'
 
     @staticmethod
     def get_series(series):
@@ -50,7 +68,7 @@ class Command(BaseCommand):
             division = Division.objects.get(code='{}{}'.format(
                 datum['state'],
                 datum['county']
-            ))
+            ), level=COUNTY_LEVEL)
             CensusEstimate.objects.update_or_create(
                 division=division,
                 variable=variable,
@@ -61,20 +79,25 @@ class Command(BaseCommand):
         except:
             print ('ERROR: {}, {}'.format(datum['NAME'], datum['state']))
 
-    def get_estimates_by_county_state(self, api, table, variable, estimate):
-        for state in tqdm(Division.objects.filter(level=STATE_LEVEL)):
-            county_data = api.get(
-                ('NAME', estimate),
-                {
-                    'for': 'county:*',
-                    'in': 'state:{}'.format(state.code)
-                },
-                year=table.year
-            )
-            for datum in county_data:
-                self.write_estimate(table, variable, estimate, datum)
+    def get_county_estimates_by_state(
+        self, api, table, variable, estimate, state
+    ):
+        """
+        Calls API for all counties in a state and a given estimate.
+        """
+        state = Division.objects.get(level=STATE_LEVEL, code=state)
+        county_data = api.get(
+            ('NAME', estimate),
+            {
+                'for': 'county:*',
+                'in': 'state:{}'.format(state.code)
+            },
+            year=table.year
+        )
+        for datum in county_data:
+            self.write_estimate(table, variable, estimate, datum)
 
-    def fetch_census_data(self):
+    def fetch_census_data(self, states):
         """
         Fetch census estimates from table.
         """
@@ -86,21 +109,27 @@ class Command(BaseCommand):
                     table.code,
                     variable.code
                 )
-                print('>> {} - {}'.format(estimate, table.year))
-                self.get_estimates_by_county_state(
-                    api=api,
-                    table=table,
-                    variable=variable,
-                    estimate=estimate
-                )
+                print('>> Fetching {} {} - {}'.format(
+                    table.year,
+                    table.series,
+                    estimate
+                ))
+                for state in tqdm(states):
+                    self.get_county_estimates_by_state(
+                        api=api,
+                        table=table,
+                        variable=variable,
+                        estimate=estimate,
+                        state=state,
+                    )
 
     @staticmethod
-    def aggregate_variable(estimate, fips):
+    def aggregate_variable(estimate, code):
         """
         Aggregate census table variables by a custom label.
         """
         estimates = [
-            variable.estimates.get(division__code=fips).estimate
+            variable.estimates.get(division__code=code).estimate
             for variable in estimate.variable.label.variables.all()
         ]
         method = estimate.variable.label.aggregation
@@ -114,31 +143,89 @@ class Command(BaseCommand):
             aggregate = None
         return aggregate
 
-    def export_data(self):
-        print("Exporting data")
+    def aggregate_counties(self, parent):
+        """
+        Aggregates county-level estimates within a given state.
+
+        Creates data structure designed for an export in this format:
+        .../{series}/{year}/{state}/{table}.json
+        """
         data = {}
-        for division in tqdm(Division.objects.all()):
-            data[division.code] = {}
+        for division in tqdm(
+            Division.objects.filter(level=COUNTY_LEVEL, parent=parent)
+        ):
+            fips = division.code
             aggregated_labels = []  # Keep track of already agg'ed variables
             for estimate in division.census_estimates.all():
+                series = estimate.variable.table.series
+                year = estimate.variable.table.year
                 table = estimate.variable.table.code
-                if table not in data[division.code]:
-                    data[division.code][table] = {}
-                label = estimate.variable.label
+                label = estimate.variable.label.label
+                code = estimate.variable.code
+                if series not in data:
+                    data[series] = {}
+                if year not in data[series]:
+                    data[series][year] = {}
+                if table not in data[series][year]:
+                    data[series][year][table] = {}
+                if fips not in data[series][year][table]:
+                    data[series][year][table][fips] = {}
                 if label is not None:
                     if label not in aggregated_labels:
-                        aggregated_labels.append(estimate.variable.label)
-                        data[division.code][table][label.label] = \
-                            self.aggregate_variable(estimate, division.code)
+                        aggregated_labels.append(label)
+                        data[series][year][table][fips][label] \
+                            = self.aggregate_variable(estimate, fips)
                 else:
-                    data[division.code][table][estimate.variable.code] = \
-                        estimate.estimate
-            # TODO: write exports...
-            # export(data[fips.code], fips.code, "census/fips")
-            # export(data, 'all', 'census')
+                    data[series][year][table][division.code][code] \
+                        = estimate.estimate
+        return data
+
+    @staticmethod
+    def export_state_files(bucket, state, data):
+        for series in data.keys():
+            for year in data[series].keys():
+                for table in data[series][year].keys():
+                    key = os.path.join(
+                        OUTPUT_PATH,
+                        series,
+                        year,
+                        state.code,
+                        '{}.json'.format(table)
+                    )
+                    bucket.put_object(
+                        Key=key,
+                        ACL='public-read',
+                        Body=json.dumps(data[series][year][table]),
+                        CacheControl=CACHE_HEADER,
+                        ContentType='application/json'
+                    )
+
+    def export_by_state(self, states):
+        bucket = get_bucket()
+        for fips in states:
+            state = Division.objects.get(level=STATE_LEVEL, code=fips)
+            print('>> Exporting state: {}'.format(state.code))
+            state_data = self.aggregate_counties(state)
+            self.export_state_files(bucket, state, state_data)
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'states',
+            nargs='+',
+            help="States to export by FIPS code. Use '+' for all."
+        )
+        parser.add_argument(
+            '--export',
+            action='store_true',
+            dest='export',
+            default=False,
+            help="Export existing estimates for states only. "
+            "Don't fetch estimates from Census API."
+        )
 
     def handle(self, *args, **options):
-        print("Beginning census run")
-        self.fetch_census_data()
-        self.export_data()
+        states = options['states']
+        if options['export'] is False:
+            self.fetch_census_data(states)
+        self.export_by_state(states)
         print("Done.")
