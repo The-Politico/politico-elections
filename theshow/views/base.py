37b4d12d91201ec96358cf24ce70df7b54fc1571
Tyler Fisher <tfisher@politico.com>
2017-11-02 11:55:12 -0400

    template paths

--- theshow/views/base.py
+++ theshow/views/base.py
@@ -4,11 +4,15 @@ Base context for all pages, e.g., data needed to render navigation.
 
 from django.views.generic import DetailView, TemplateView
 
+from core.aws import defaults
+
 
 class BaseView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(BaseView, self).get_context_data(**kwargs)
+        context['domain'] = defaults.DOMAIN
+        context['root_path'] = defaults.ROOT_PATH
         # Context TK
         return context
 
