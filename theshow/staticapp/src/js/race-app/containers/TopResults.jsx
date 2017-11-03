73a36e69b3a93c8e0682ed02efd349188fdc94bf
Lily Mihalik <lmihalik@C02RX9B8G8WN.local>
2017-11-03 11:10:38 -0400

    Adding the ad127761277612776 and tweaking the top bar

--- theshow/staticapp/src/js/race-app/containers/TopResults.jsx
+++ theshow/staticapp/src/js/race-app/containers/TopResults.jsx
@@ -5,18 +5,24 @@ import ResultsMap from '../components/ResultsMap';
 
 const TopResults = (props) => {
   return (
-    <div className="top-results row-fluid content-extra-large section">
-      <div className="loading-bar">
-        <p>Checking for results bar</p>
+    <div className="top-results row-fluid section">
+    <div class="content-extra-large">
+        <div className="loading-bar">
+          <p>Checking for results bar</p>
+        </div>
+        <div className="bar">
+          <ResultsBar session={props.session} />
+        </div>
+        <div className="map">
+          <ResultsMap session={props.session} />
+        </div>
+        <div class="clear"></div>
+    </div>
+      <div class="content-group ad">
+          <p>Advertisement</p>
+          <div class="ad-slot flex horizontal" id="pol-06" ></div>
       </div>
-      <div className="bar">
-        <ResultsBar session={props.session} />
-      </div>
-      <div className="map">
-        <ResultsMap session={props.session} />
-      </div>
-      <div class="clear"></div>
-      <p class="text-align-center"> !!!!AD cube ad here!!!! </p>
+
     </div>
   );
 };
