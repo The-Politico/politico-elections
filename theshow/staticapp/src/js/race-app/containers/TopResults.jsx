ef8ffbad5af0f29a869b6a5464fc37c6d6eb71ad
Lily Mihalik <lmihalik@C02RX9B8G8WN.local>
2017-11-03 10:06:02 -0400

    reworking the top level bar

--- theshow/staticapp/src/js/race-app/containers/TopResults.jsx
+++ theshow/staticapp/src/js/race-app/containers/TopResults.jsx
@@ -6,10 +6,10 @@ import ResultsMap from '../components/ResultsMap';
 const TopResults = (props) => {
   return (
     <div className="top-results row-fluid content-extra-large section">
-      <div className="bar col-sm-5">
+      <div className="bar">
         <ResultsBar session={props.session} />
       </div>
-      <div className="map col-sm-7">
+      <div className="map">
         <ResultsMap session={props.session} />
       </div>
       <div class="clear"></div>
