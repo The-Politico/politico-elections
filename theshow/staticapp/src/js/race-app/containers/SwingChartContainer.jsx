3a09e9e31bd2afb54796d9a33c5145d401964582
lmihalik <lilyinchile@gmail.com>
2017-11-05 13:01:54 -0500

    shorter titles

--- theshow/staticapp/src/js/race-app/containers/SwingChartContainer.jsx
+++ theshow/staticapp/src/js/race-app/containers/SwingChartContainer.jsx
@@ -7,7 +7,7 @@ const SwingChartContainer = (props) => {
   return (
     <div className="swing-chart row-fluid section">
     <div class="content-large">
-      <div class="section-header-wrap"><div class="header-line"></div><h2 class="section-title">How did voters shift from 2016?</h2></div>
+      <div class="section-header-wrap"><div class="header-line"></div><h2 class="section-title">Vote shift from 2016</h2></div>
       <SwingChart session={props.session} />
     </div>
     <div class="content-group ad">
