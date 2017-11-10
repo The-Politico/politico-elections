680877d07857a1c1843efc56d8b3064f5a6910c5
Tyler Fisher <tfisher@politico.com>
2017-11-10 09:50:47 -0500

    inline candidateelections on elections

--- election/admin.py
+++ election/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 
-from election.models import (Candidate, Election, CandidateElection, ElectionCycle, ElectionDay,
-                             ElectionType, Party, Race)
+from election.models import (Candidate, Election, CandidateElection,
+                             ElectionCycle, ElectionDay, ElectionType,
+                             Party, Race)
+
+
+class CandidateElectionInline(admin.StackedInline):
+    model = CandidateElection
+
+
+class ElectionAdmin(admin.ModelAdmin):
+    inlines = [
+        CandidateElectionInline
+    ]
 
 admin.site.register(Race)
 admin.site.register(Party)
-admin.site.register(Election)
+admin.site.register(Election, ElectionAdmin)
 admin.site.register(CandidateElection)
 admin.site.register(ElectionDay)
 admin.site.register(ElectionType)
